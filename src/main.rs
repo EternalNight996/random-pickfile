3194ae6b66de3610d8e43aa0be4318abc1af9e0c
1379 <EternalNightYeah2@yeah.net>
2022-07-05 02:17:53 +0800

    fix bug

--- src/main.rs
+++ src/main.rs
@@ -25,48 +25,16 @@ fn main() -> Result<(), Error> {
     let date: String = Local::now().format(&settings.dir.date).to_string();
     // 获取文件格式列表
     let file_type: Vec<String> = serde_json::from_str(&settings.file.filetypes).unwrap();
-    let mut dir_list: Vec<(usize, PathBuf)> = vec![];
-    // 非目标
-    let mut other_list: Vec<PathBuf> = vec![];
     output!("finding filetype -> {:?}", file_type);
     // 目标目录的前缀
     let dir_prefix: String = format!(
         "{}{}{}{}{}",
         settings.dir.workspace, PATH_KEY, date, settings.dir.name, DIR_SUFFIX_FLAG
     );
-    // 防止重复名称标识
-    let mut dir_suffix_num: i32 = 1;
-    match fs::read_dir(settings.dir.workspace.clone()) {
-        Err(e) => {
-            output!("read path Error: {:#?}", e.kind());
-            return Err(e);
-        }
-        Ok(paths) => {
-            for p in paths {
-                if let Ok(data) = p {
-                    let d = data.path();
-                    if let Some(tmp) = d.to_str() {
-                        if tmp.contains(DIR_SUFFIX_FLAG) {
-                            dir_suffix_num += 1;
-                        };
-                        // 获取地址并匹配对应格式
-                        if let Some(index) = file_type
-                            .iter()
-                            .position(|x| tmp.contains(&format!(".{}", *x)))
-                        {
-                            // 添加文件路径
-                            dir_list.push((index, d));
-                        } else {
-                            other_list.push(d);
-                        }
-                    }
-                }
-            }
-        }
-    }
-
+    let (dir_suffix_num, mut file_list, other_list) =
+        get_dir_file(&settings.dir.workspace, file_type.clone()).unwrap();
     // 获取文件数据
-    let mut list_num: usize = dir_list.len();
+    let mut list_num: usize = file_list.len();
     if list_num > 0 {
         // 判断是否需要建立大于1的文件夹
         let f_num: f32 = list_num as f32 / settings.dir.pick_count;
@@ -88,7 +56,7 @@ fn main() -> Result<(), Error> {
                 } else {
                     0
                 };
-                let from_path = dir_list.swap_remove(random_num);
+                let from_path = file_list.swap_remove(random_num);
                 let random_name = format!("{}{}", random!(nanoid 12), file_type[from_path.0]);
                 let from_name = if settings.file.is_rename {
                     random_name
@@ -108,7 +76,7 @@ fn main() -> Result<(), Error> {
                     path::PathBuf::from(from_path.1.display().to_string()),
                     &to_path,
                 )?;
-                list_num = dir_list.len();
+                list_num = file_list.len();
                 if list_num == 0 {
                     break;
                 }
@@ -133,49 +101,29 @@ fn main() -> Result<(), Error> {
                 "trying check -> {} whether exists dir...",
                 settings.dir.workspace
             );
-            match fs::read_dir(settings.dir.workspace.clone()) {
-                Err(e) => {
-                    return Err(e);
+            // 防止重复名称标识
+            let dir_list = get_dir_file(&settings.dir.workspace, vec![])
+                .and_then(|(_, _, x)| {
+                    Ok(x.into_iter()
+                        .filter_map(|xx| if xx.is_dir() { Some(xx) } else { None })
+                        .collect::<Vec<PathBuf>>())
+                })
+                .unwrap();
+            println!("check list {:?}", other_list);
+            if dir_list.len() > 0 {
+                output!("finded [`{}`] dir, trying compress of zip.", dir_list.len());
+                for src_dir in dir_list {
+                    let dst_dir = PathBuf::from(&format!("{}{}", src_dir.display(), ".zip"));
+                    output!(zip_compress_dir(
+                        &src_dir,
+                        &dst_dir,
+                        settings.dir.compress_level,
+                        settings.dir.is_save_compress_dir,
+                        settings.dir.compress_method.clone()
+                    ));
                 }
-                Ok(paths) => {
-                    let mut dir_list = vec![];
-                    for p in paths {
-                        if let Ok(data) = p {
-                            let d = data.path();
-                            if d.is_dir() {
-                                let split = d
-                                    .file_name()
-                                    .and_then(|x| x.to_str())
-                                    .unwrap_or("")
-                                    .to_string();
-                                if !split.is_empty() {
-                                    dir_list.push(split);
-                                }
-                            }
-                        }
-                    }
-                    println!("check list {:?}", dir_list);
-                    if dir_list.len() > 0 {
-                        output!("finded [`{}`] dir, trying compress of zip.", dir_list.len());
-                        for src in dir_list {
-                            let src_dir = PathBuf::from(&format!(
-                                "{}{}{}",
-                                settings.dir.workspace, PATH_KEY, src
-                            ));
-                            let dst_dir =
-                                PathBuf::from(&format!("{}{}", src_dir.display(), ".zip"));
-                            output!(zip_compress_dir(
-                                &src_dir,
-                                &dst_dir,
-                                settings.dir.compress_level,
-                                settings.dir.is_save_compress_dir,
-                                settings.dir.compress_method.clone()
-                            ));
-                        }
-                    } else {
-                        output!("Couldn't find dir of {}", settings.dir.workspace);
-                    }
-                }
+            } else {
+                output!("Couldn't find dir of {}", settings.dir.workspace);
             }
         }
     }
@@ -183,6 +131,36 @@ fn main() -> Result<(), Error> {
     Ok(())
 }
 
+/// 获取目标目录数据
+pub fn get_dir_file(
+    workspace: &str,
+    file_type: Vec<String>,
+) -> Result<(i32, Vec<(usize, PathBuf)>, Vec<PathBuf>), Error> {
+    let mut res: (i32, Vec<(usize, PathBuf)>, Vec<PathBuf>) = (1, vec![], vec![]);
+
+    let paths = fs::read_dir(workspace)?;
+    for p in paths {
+        if let Ok(data) = p {
+            let d = data.path();
+            if let Some(tmp) = d.to_str() {
+                if tmp.contains(DIR_SUFFIX_FLAG) {
+                    res.0 += 1;
+                };
+                // 获取地址并匹配对应格式
+                if let Some(index) = file_type
+                    .iter()
+                    .position(|x| tmp.contains(&format!(".{}", *x)))
+                {
+                    // 添加文件路径
+                    res.1.push((index, d));
+                } else {
+                    res.2.push(d);
+                }
+            }
+        }
+    }
+    Ok(res)
+}
 /// 移动文件
 pub fn move_file_fn(from: PathBuf, to: &PathBuf) -> Result<(), Error> {
     output!("from: {} -> to: {}", from.display(), to.display());
